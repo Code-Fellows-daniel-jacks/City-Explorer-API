d07f4af37e2ec2ac168cd76cb1cf61d931ac714e
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-12-07 13:47:31 -0800

    sets up city explorer api

--- server.js
+++ server.js
@@ -4,11 +4,40 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-
+const weatherData = require('./data/weather.json')
 const app = express();
 
 app.use(cors());
 
 const PORT = process.env.PORT
+
+app.get('/test', function (request, response) {
+    response.send('test works');
+})
+
+
+app.get('/weather-data', function getWeatherData(request, response) {
+    response.send(weatherData[0]);
+})
+
+// class LocationData {
+//     constructor(obj) {
+//         this.searchQuery = obj.
+//             this.lat = obj.data
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => console.log('server is listening on ', PORT))
